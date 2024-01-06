5419a554c03e3a48ccf2aadf265df9039b3f702f
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-06 15:15:28 +0530

    done

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -25,6 +25,19 @@ export default class AppClass extends Component{
     ]
     return data;
   }
-
   // code here
+  render(){
+    this.data = this.imageData()
+    console.log(this.data)
+    return (
+      <>
+      <div className='heading'>Kalvium Gallary</div>
+      <div className='container'>
+        {this.data.map((e)=>(
+          <img key={e.id} className='image' src={e.img} />
+        ))}
+      </div>
+      </>
+    )
+  }
 }
