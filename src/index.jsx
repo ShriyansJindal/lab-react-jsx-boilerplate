34e9d3502edb03fda44cd86ec59ae7f2e174e289
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-06 15:16:44 +0530

    done

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import AppClass from "./AppClass";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
+    {/* <App />    */}
     {/* change the above line - for interchaning between App and AppClass */}
     <AppClass/>
   </React.StrictMode>
